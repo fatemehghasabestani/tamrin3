db09c1cd757cea491dc281e4efc7822815905e7b
Fatemeh Ghasabestani <f6945671@gmail.com>
2025-12-14 22:16:20 +0330

    create pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PrivateTemplate } from './+private/private-template/private-template';
+import { LoginPage } from './+public/+pages/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:LoginPage},
+    {path:'private',component:PrivateTemplate,children:[
+
+    ]},
+    {path:'',redirectTo:'login',pathMatch:'full'},
+    {path:'**',redirectTo:'login'}
+];
