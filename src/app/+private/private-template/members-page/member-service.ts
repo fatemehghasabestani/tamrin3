524722a3eea41973552260d1687d2b2d60b93577
Fatemeh Ghasabestani <f6945671@gmail.com>
2025-12-21 16:55:42 +0330

    end of member table

---
+++ src/app/+private/private-template/members-page/member-service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { MembersItem } from './members-page';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+    private data: MembersItem[] = [
+      { id: 1, firstname: 'فاطمه', lastname: 'خزایی', nationalid: 3950695133, gender: 'مونث', mobilenumber: 9309908765, address: 'همدان  ', membershiptype: 'عادی' },
+      { id: 2, firstname: 'محمد', lastname: 'سلگی', nationalid: 3960876543, gender: 'مذکر', mobilenumber: 9112345654, address: 'تهران', membershiptype: 'دانشجویی' },
+      { id: 3, firstname: 'لیلا', lastname: 'زمانی', nationalid: 5678900987, gender: 'مونث', mobilenumber: 9367890987, address: 'نهاوند', membershiptype: 'ویژه' },
+      { id: 4, firstname: 'رضا', lastname: 'رمضانی', nationalid: 7654322343, gender: 'مذکر', mobilenumber: 9197654567, address: 'شیراز', membershiptype: 'عادی' },
+      { id: 5, firstname: 'پریا', lastname: 'مومیوند', nationalid: 3958790523, gender: 'مونث', mobilenumber: 9301298765, address: 'مشهد', membershiptype: 'دانشجویی' },
+    ];
+    list(){
+      return [...this.data];
+    }
+    add(item:MembersItem){
+      this.data.push(item);
+    }
+}
--- src/app/+private/private-template/members-page/members-page.ts
+++ src/app/+private/private-template/members-page/members-page.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MemberService } from './member-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-members-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './members-page.html',
   styleUrl: './members-page.scss',
 })
-export class MembersPage {
+export class MembersPage implements OnInit {
+  save() {
+      this.memberService.add(this.item);
+   this.dataRefresh();
+   this.state='list';
+
+  }
+  ngOnInit(): void {
+    this.dataRefresh();
+  }
+  data: MembersItem[] = [];
+  item:MembersItem={
+    id:0,
+    firstname:'',
+    lastname:'',
+    nationalid:0,
+    gender:'',
+    mobilenumber:0,
+    address:'',
+    membershiptype:'',
+  };
+  memberService = inject(MemberService);
+  state: string = 'list';
+  dataRefresh() {
+    this.data = this.memberService.list();
+  }
+
+  add() {
+   this.state='add';
+  
+  }
+  cancel(){
+    this.state='list';
+  }
+}
+export interface MembersItem {
+  id: number;
+  firstname: string;
+  lastname: string;
+  nationalid: number;
+  gender: string;
+  mobilenumber: number;
+  address: string;
+  membershiptype: string;
 
 }
